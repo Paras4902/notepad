0b031f40d82d9210c4c7c6987e31c0fad20811e0
Paras Udapurkar <66427187+Paras4902@users.noreply.github.com>
2020-11-22 23:26:29 +0530

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -8,39 +8,23 @@ from datetime import datetime
 import webbrowser
 
 
-# ======================================================================================
-#  ========================== File Code Starts Here  ============================
-# =======================================================================================
-
-
-# =================================== New Code  ======================================
 def new():
     text.delete('1.0', 'end')
 
 
-# ===================================== End =========================================
-
-
-# ========================= New Window Code  ================================
 def new_window():
     root = tk.Tk()
     root.geometry('500x500')
-
     menu_bar = Menu(root)
-
     filed = Menu(menu_bar, tearoff=0)
     filed.add_command(label="New", command=new)
     filed.add_command(label="New window", command=new_window)
     filed.add_command(label="Open", command=openn)
-    filed.add_command(label="Save", command=save)
     filed.add_command(label="Save as", command=save_as)
     filed.add_separator()
     filed.add_command(label="Exit", command=exitt)
     menu_bar.add_cascade(label="File", menu=file, font=('verdana', 10, 'bold'))
-
     editt = Menu(menu_bar, tearoff=0)
-
-    editt.add_command(label="Undo", command=undo)
     editt.add_separator()
     editt.add_command(label="Cut", command=cut)
     editt.add_command(label="Copy", command=copy)
@@ -49,34 +33,21 @@ def new_window():
     editt.add_command(label="Select All", accelerator="Ctrl+A", command=select_all)
     editt.add_command(label="Time/Date", accelerator="F5", command=time)
     menu_bar.add_cascade(label="Edit", menu=edit)
-
     formatt = Menu(menu_bar, tearoff=0)
-
     formatt.add_command(label="Word Wrap")
     formatt.add_command(label="Font...", command=fonts)
-
     menu_bar.add_cascade(label="Format", menu=formatt)
-
     helpp = Menu(menu_bar, tearoff=0)
-
     helpp.add_command(label="View Help", command=view_help)
     helpp.add_command(label="Send FeedBack", command=send_feedback)
     helpp.add_command(label="About Notepad")
-
     menu_bar.add_cascade(label="Help", menu=helpp)
-
     root.config(menu=menu_bar)
-
     textt = ScrolledText(root, width=1000, height=1000)
     textt.place(x=0, y=0)
-
     root.mainloop()
 
 
-# =========================== End ==============================================
-
-
-# ===================== Open File Code ========================================
 def openn():
     window.filename = filedialog.askopenfilename(
         initialdir='/',
@@ -86,17 +57,6 @@ def openn():
     text.insert('end', filee.read())
 
 
-# ================================= End ==========================================
-
-
-# ================================ Save File Code ====================================
-def save():
-    pass
-
-
-# ================================    End      =======================================
-
-# =================================== save as File code  ==============================
 def save_as():
     window.filename = filedialog.asksaveasfile(mode="w", defaultextension='.txt')
     if window.filename is None:
@@ -106,9 +66,6 @@ def save_as():
     window.filename.close()
 
 
-# ================================ End ============================================
-
-# ================================ Exit Code =====================================
 def exitt():
     message = messagebox.askquestion('Notepad', "Do you want to save changes")
     if message == "yes":
@@ -117,46 +74,18 @@ def exitt():
         window.destroy()
 
 
-# ==================================== end =========================================
-
-
-# ======================================================================================
-# ======================= Edit Code Starts Here  ============================
-# =======================================================================================
-
-
-# =========================== Undo code =============================
-def undo():
-    pass
-
-
-# Its Challenge for you to give the undo functionality in the notepad
-
-# =========================== End code =====================================
-
-# =========================== Cut code =============================
 def cut():
     text.event_generate("<<Cut>>")
 
 
-# =========================== End code =====================================
-
-# =========================== Cut code =============================
 def copy():
     text.event_generate("<<Copy>>")
 
 
-# =========================== End code =====================================
-
-# =========================== Cut code =============================
 def paste():
     text.event_generate("<<Paste>>")
 
 
-# =========================== End code =====================================
-
-
-# =========================== Delete all code =============================
 def delete():
     message = messagebox.askquestion('Notepad', "Do you want to Delete all")
     if message == "yes":
@@ -165,42 +94,20 @@ def delete():
         return "break"
 
 
-# =========================== End code =====================================
-
-
-# =========================== select all code =============================
 def select_all():
     text.tag_add('sel', '1.0', 'end')
     return 'break'
 
 
-# =========================== End code =============================
-
-
-# =========================== Time/Date code =============================
 def time():
     d = datetime.now()
     text.insert('end', d)
-
-
-# =========================== End code =============================
-
-
-# ======================================================================================
-# ======================= Edit Code Ends Here  ============================
-# =======================================================================================
-
-
-# ======================================================================================
-# ======================= Format Code Starts Here  ============================
-# =======================================================================================
 
 
 def fonts():
     root = tk.Tk()
     root.geometry('400x400')
     root.title('Font')
-
     l1 = Label(root, text="Font:")
     l1.place(x=10, y=10)
     f = tk.StringVar()
@@ -208,7 +115,6 @@ def fonts():
     fontss['values'] = font.families()
     fontss.place(x=10, y=30)
     fontss.current(0)
-
     l2 = Label(root, text="Font Style:")
     l2.place(x=180, y=10)
     st = tk.StringVar()
@@ -216,20 +122,16 @@ def fonts():
     style['values'] = ('bold', 'bold italic', 'italic')
     style.place(x=180, y=30)
     style.current(0)
-
     l3 = Label(root, text="Size:")
     l3.place(x=350, y=10)
     sz = tk.StringVar()
     size = ttk.Combobox(root, width=2, textvariable=sz, state='readonly', font=('verdana', 10, 'bold'), )
-
     size['values'] = (8, 9, 10, 12, 15, 20, 23, 25, 27, 30, 35, 40, 43, 47, 50, 55, 65, 76, 80, 90, 100, 150, 200, 255, 300)
     size.place(x=350, y=30)
     size.current(0)
-
     sample = LabelFrame(root, text="Sample", height=100, width=200)
     sample['font'] = (fontss.get(), size.get(), style.get())
     sample.place(x=180, y=220)
-
     l4 = Label(sample, text="This is sample")
     l4.place(x=20, y=30)
 
@@ -258,55 +160,28 @@ def about():
     messagebox.showinfo("About", "This Notepad is created by Paras Udapurkar")
 
 
-# ======================================================================================
-# ======================= Format Code Ends Here  ============================
-# =======================================================================================
-
-# ======================================================================================
-# ======================= Help Code Ends Here  ============================
-# =======================================================================================
-
-# ======================   View Help ===================================
 def view_help():
     webbrowser.open('#')
 
 
-# ============================= End =======================================
-
-# ======================   View Help ===================================
 def send_feedback():
     webbrowser.open('#')
 
 
-# ============================= End =======================================
-
-
-# ======================================================================================
-# ======================= Help Code Ends Here  ============================
-# =======================================================================================
-
-
-# ============================= Main Window =============================
-
 window = tk.Tk()
-window.geometry('500x500')
+window.geometry('1000x500')
 window.title('notepad')
 window.iconbitmap('notepad.ico')
 menubar = Menu(window)
-
 file = Menu(menubar, tearoff=0)
 file.add_command(label="New", command=new)
 file.add_command(label="New window", command=new_window)
 file.add_command(label="Open", command=openn)
-file.add_command(label="Save", command=save)
 file.add_command(label="Save as", command=save_as)
 file.add_separator()
 file.add_command(label="Exit", command=exitt)
 menubar.add_cascade(label="File", menu=file, font=('verdana', 10, 'bold'))
-
 edit = Menu(menubar, tearoff=0)
-
-edit.add_command(label="Undo", accelerator="Ctrl+Z", command=undo)
 edit.add_separator()
 edit.add_command(label="Cut", accelerator="Ctrl+X", command=cut)
 edit.add_command(label="Copy", accelerator="Ctrl+C", command=copy)
@@ -315,27 +190,16 @@ edit.add_command(label="Delete", accelerator="Del", command=delete)
 edit.add_command(label="Select All", accelerator="Ctrl+A", command=select_all)
 edit.add_command(label="Time/Date", accelerator="F5", command=time)
 menubar.add_cascade(label="Edit", menu=edit)
-
 Format = Menu(menubar, tearoff=0)
-
 Format.add_command(label="Word Wrap")
 Format.add_command(label="Font...", command=fonts)
-
 menubar.add_cascade(label="Format", menu=Format)
-
 Help = Menu(menubar, tearoff=0)
-
 Help.add_command(label="View Help", command=view_help)
 Help.add_command(label="Send FeedBack", command=send_feedback)
 Help.add_command(label="About Notepad", command=about)
-
 menubar.add_cascade(label="Help", menu=Help)
-
 window.config(menu=menubar)
-
 text = ScrolledText(window, width=1000, height=1000)
 text.place(x=0, y=0)
-
 window.mainloop()
-
-# ========================== End =======================================
